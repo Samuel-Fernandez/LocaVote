4ca9e9afd6f3f1b691352c8ae6b2ca0eb81462a7
heigSamFernande <samuel.fernandez@heig-vd.ch>
2025-04-29 14:23:41 +0200

    Skeleton of the project

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return "Hello World"
+
+@main.route('/about')
+def about():
+    return "About"
